d615182a13db9f4d9d726f188585505badc476cb
Scott Zelenka <szelenka@cisco.com>
2020-01-03 21:31:56 -0500

    migrating to new pc

--- src/DataframeHelper.py
+++ src/DataframeHelper.py
@@ -3,14 +3,14 @@ import json
 import pandas as pd
 
 
-def merge_dataframes(df_mhe: pd.core.frame.DataFrame, df_pvw: pd.core.frame.DataFrame, dc_ac_loss: float = 0.15):
+def merge_dataframes(df_mhe: pd.core.frame.DataFrame, df_pvw: pd.core.frame.DataFrame, dc_ac_loss: float = 0.1):
     df = df_mhe.merge(df_pvw, on='month_int', how='outer').sort_values('date').dropna().reset_index(drop=True)
     df['kwh_demand'] = df['avg_kwh_per_day'] * (1 + dc_ac_loss) / df['radiation_hours_per_day'] 
     
     return df
 
 
-def avg_month(df: pd.core.frame.DataFrame, solar_system_kwh: float = 6.32, dc_ac_loss: float = 0.15):
+def avg_month(df: pd.core.frame.DataFrame, solar_system_kwh: float = 6.32, dc_ac_loss: float = 0.1):
     # Duke Progress resets the year on May-31, meaning any unused credits before then are removed from the account
     m = df.groupby('month_int').agg('mean').reindex([6,7,8,9,10,11,12,1,2,3,4,5])
     m['kwh_per_day_delta'] = m['avg_kwh_per_day'] - (
@@ -23,7 +23,9 @@ def avg_month(df: pd.core.frame.DataFrame, solar_system_kwh: float = 6.32, dc_ac
     return m
 
 
-def annual_costs(df: pd.core.frame.DataFrame):
+def annual_costs(df: pd.core.frame.DataFrame, duke_fee=11.8):
+    # Customers are separately responsible for the monthly $11.80 Basic Facilities Charge.
+    # https://www.energysage.com/net-metering/duke-energy/
     def net_meter(series):
         credits = 0.
         expense = 0.
@@ -32,12 +34,16 @@ def annual_costs(df: pd.core.frame.DataFrame):
                 if credits == 0:
                     expense += s
                 else:
-                    credits -= s
+                    credits -= s 
                     if credits < 0:
                         expense += -1 * credits
                         credits = 0
+                    # Duke Interconnect fee is around $12/month even with zero usage
+                    expense += duke_fee
             else:
                 credits += -1 * s
+                # Duke Interconnect fee is around $12/month even with zero usage
+                expense += duke_fee
 
         return expense, credits
 
